928696b5788d1bc8c2b6d9657deed9997aa69f07
Tillink <41276552+Tillink@users.noreply.github.com>
2018-10-14 16:29:46 +0900

    Update Database.cs
    asd

--- Database.cs
+++ Database.cs
@@ -8,5 +8,55 @@ namespace RpgmakerScripts
 {
     class Database
     {
+        // Left Pannel
+        // top    :  ClassesList      // TextBox : Class name
+        // middle :  ListSort         // Show : All member in list
+        // bottom :  ListModifyNumber // Button : Change Maximum
+        protected void FirstPannel(Database dblist)
+        {
+            // class name
+            dblist.ToString();
+
+            //dblist sort
+            List<Database> list = dblist;
+            list.Sort();
+
+            // Change Maximum Button
+            // 1. max : number (scroll)
+            // 2. OK, Cancel (IsSave)
+
+            int max; // min :1- max: 999
+            bool IsSave = false;
+            // if push button ok, return save; else return;
+        }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        /// <summary>
+        /// General Settings
+        /// Basic settings
+        /// </summary>
+        protected virtual void GeneralSettings()
+        {
+            Name = Console.ReadLine();
+            Description = Console.ReadLine();
+        }
+        // Center Pannel
+        // top : General Settings
+        //          :Actors, Classes, Skills, Items, Weapons, Armors, Enemies, Troops, States, Animations, Tilesets, CommonEvents
+
+        // 3. 상태 정보, 메모창
+        // use override method
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual Features()
+        {
+            //      Kind, Content
+            //ex)   State Rate,
+            //       [some State] * [rate]%
+        }
+        // Right Pannel
+        //top    :  Features
+        //bottom :  Note
     }
 }
